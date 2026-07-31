d85270dbfb1411a39ae5f8f3910bdcc0e28e6fb3
williamas47 <williamas66@gmail.com>
2026-07-31 09:23:15 -0300

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,7 +29,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='pt-BR' className={`${geistSans.variable} h-full antialiased`}>
-      <body className='min-h-full flex flex-col'>{children}</body>
+      <body className='min-h-full flex flex-col'>
+        {children}
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=AW-1037137125'
+          strategy='afterInteractive'
+        />
+        <Script id='google-tag' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-1037137125');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
